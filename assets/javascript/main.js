e10bab9a4cec6a940769bf955ac7b1beed0a8e86
katie stout <katiestout@katies-MBP.home>
2018-06-04 22:09:51 -0400

    it's working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,7 @@
 
   
-
+//topics array, we will dynamically create some buttons for this! and
+//push more buttons in the array!
   var topics = ['mean girls', 'Bob Ross','Jersey Shore', 'ducks', 
 'figure skating','barbie', 'sound of music'];
 
@@ -8,17 +9,17 @@
 
 
 function displayGif(){
-
+//empty 
     $("#newGifs").empty();
 
     var topic = $(this).attr("data-name");
 
-
+//query url with api key
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     topic + "&api_key=C3PI5AGfGcUmb8dIKbGlyuFoUh08PbKO";
 
 
-
+//ajax call!
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -26,7 +27,12 @@ function displayGif(){
       
       .then(function(response) {
 
-        //console.log(response);
+        console.log(response);
+
+        //I was thinking to limit the responses to use .slice or .val
+        //results.slice (0,10);
+        //$(results).val(10);
+
         //storing array of reults in the results variable
       var results = response.data;
       //loop!
@@ -38,6 +44,7 @@ function displayGif(){
          var imgURL = results[i].images.fixed_height_still.url;
 
             var image = $("<img>").attr("src", imgURL);
+            //add gif class
 
             $(image).addClass("gif");
 
@@ -51,8 +58,10 @@ function displayGif(){
 
 function makeButtons (){
 $("#newButtons").empty();
+//empty the div
 
 for (var i = 0; i < topics.length; i++){
+    //loop over the topics you already made, and make buttons for them
 
     var a = $("<button>");
 
@@ -64,7 +73,7 @@ for (var i = 0; i < topics.length; i++){
 }
 
 }
-
+//onclick event for the gif input
 $("#add-gif").on("click", function(event){
     event.preventDefault();
     var topic = $("#gif-input").val().trim();
@@ -72,6 +81,13 @@ $("#add-gif").on("click", function(event){
     makeButtons();
 });
 
+//necessary to make the gifs appear
+
 $(document).on("click", ".gif-btn", displayGif);
 
 makeButtons();
+
+
+
+
+//add the pause and start!!!:(
